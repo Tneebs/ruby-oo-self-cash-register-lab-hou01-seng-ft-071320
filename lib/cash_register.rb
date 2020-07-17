a9551213f4237b4a06cea178556f9e96ecf6a623
Tneebs <tedneben@gmail.com>
2020-07-16 22:49:02 -0500

    Update 2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,12 @@
+require 'pry'
 
+class CashRegister
+    attr_accessor :total, :discount
+
+    def initialize(discount = 0)
+        @total = discount
+        @discount = discount
+    end
+
+
+end
